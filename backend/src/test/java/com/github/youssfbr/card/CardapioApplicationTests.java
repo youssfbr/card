395679a378f124ba714cf7be122593adc1051dda
Alisson Youssf <youssfbr@gmail.com>
2023-06-08 12:46:26 -0300

    Backend project created

---
+++ backend/src/test/java/com/github/youssfbr/card/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
